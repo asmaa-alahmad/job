36d1cdda202007b1bfb3281f5a5f75b1169824a3
asmaa-alahmad <as.n.mousa@gmail.com>
2025-11-09 19:09:19 +0300

    edit admin/shared/side_bars/cms and editadmin/shared/sidebar

--- resources/views/admin/shared/side_bars/cms.blade.php
+++ resources/views/admin/shared/side_bars/cms.blade.php
@@ -1,4 +1,4 @@
-<li class="nav-item  "> <a href="javascript:;" class="nav-link nav-toggle"> <i class="fa fa-file-text-o" aria-hidden="true"></i> <span class="title">C.M.S</span> <span class="arrow"></span> </a>
+<li class="nav-item  "> <a href="javascript:;" class="nav-link nav-toggle"> <i class="fa fa-file-text-o" aria-hidden="true"></i> <span class="title">manage content</span> <span class="arrow"></span> </a>
     <ul class="sub-menu">
         <li class="nav-item  "> <a href="{{ route('list.cms') }}" class="nav-link "> <span class="title">List C.M.S Pages</span> </a> </li>
         <li class="nav-item  "> <a href="{{ route('create.cms') }}" class="nav-link "> <span class="title">Add new C.M.S Page</span> </a> </li>
